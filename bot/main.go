09b3e201d58cdceb0f9a61076f3baf6a2a087f1f
Lezh1k <lezh1k.vohrer@gmail.com>
2019-02-06 12:04:30 +0600

    Added polling logic + changed docker-compose

--- bot/main.go
+++ bot/main.go
@@ -8,14 +8,31 @@ import (
 	"strings"
 	"time"
 
-	"github.com/tarm/serial"
-
 	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/tarm/serial"
+)
+
+const (
+	envToken  = "TELEGRAM_TOKEN"
+	envChid   = "CHANNEL_ID"
+	envSerial = "SERIAL_PORT"
+
+	defaultSerial = "/dev/rfcomm0"
+	defaultToken  = "HUYOKEN"
+	defaultChid   = 0
 )
 
 func main() {
-	token := os.Getenv("TELEGRAM_TOKEN")
-	channel, err := strconv.ParseInt(os.Getenv("CHANNEL_ID"), 10, 64)
+	token := os.Getenv(envToken)
+	if token == "" {
+		token = defaultToken
+	}
+	serialPort := os.Getenv(envSerial)
+	if serialPort == "" {
+		serialPort = defaultSerial
+	}
+
+	channel, err := strconv.ParseInt(os.Getenv(envChid), 10, 64)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -31,29 +48,36 @@ func main() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	ch, errCh := PingPlatform()
+	ch, errCh := PingPlatform(serialPort)
 
 	go func(errCh <-chan error) {
 		for err := range errCh {
 			if err != nil {
-				fmt.Println(err)
+				fmt.Printf("Something went wrong: %v", err)
 			}
 		}
 	}(errCh)
 
 	botReceivedAnswerFromUser := false
 	go func(ch <-chan bool) {
-		const fiveMinutes = 60 * 5
+		const timeoutSec = 60 * 5
 		count := 0
 		for pressed := range ch {
+			fmt.Printf("%d: Platform pressed : %v\n", count, pressed)
 			if pressed {
+				fmt.Println("Something on platform")
+				if count >= timeoutSec {
+					msg := tgbotapi.NewMessage(int64(channel), "Что-то поставили на платформу. Может воду?")
+					bot.Send(msg)
+				}
 				count = 0
 				continue
 			}
 			count++
-			if count >= fiveMinutes && !botReceivedAnswerFromUser {
+			if count >= timeoutSec && !botReceivedAnswerFromUser {
 				msg := tgbotapi.NewMessage(int64(channel), "На платформе нет воды. Засыхаем!")
 				bot.Send(msg)
+				time.Sleep(time.Minute * 1)
 			}
 		}
 	}(ch)
@@ -65,15 +89,16 @@ func main() {
 			continue
 		}
 
-		if strings.Contains(update.Message.Text, "@"+bot.Self.UserName) && strings.Contains(update.Message.Text, "заказали") && strings.Contains(update.Message.Text, "воду") && strings.Contains(update.Message.Text, "брат") {
+		if strings.Contains(update.Message.Text, "@"+bot.Self.UserName) && strings.Contains(update.Message.Text, "заказали") /*&& strings.Contains(update.Message.Text, "воду") && strings.Contains(update.Message.Text, "брат")*/ {
 			if !botReceivedAnswerFromUser {
 				go func() {
-					time.Sleep(time.Hour * 24)
+					sleepDuration := time.Hour * 24
+					time.Sleep(sleepDuration)
 					botReceivedAnswerFromUser = false
 				}()
 			}
 			botReceivedAnswerFromUser = true
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "понял тебя, брат!")
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Принято! Жду 24 часа")
 			msg.ReplyToMessageID = update.Message.MessageID
 			bot.Send(msg)
 		}
@@ -81,11 +106,11 @@ func main() {
 }
 
 //PingPlatform pings platform to get current water quantity, if unable to reach the platform, sends error
-func PingPlatform() (<-chan bool, <-chan error) {
+func PingPlatform(serialPort string) (<-chan bool, <-chan error) {
 
 	const chkCmd = 0xa6 //see hw part
 	buf := make([]byte, 8)
-	scfg := &serial.Config{Name: "/dev/rfcomm0", Baud: 9600}
+	scfg := &serial.Config{Name: serialPort, Baud: 9600}
 	chData := make(chan bool)
 	chError := make(chan error)
 
